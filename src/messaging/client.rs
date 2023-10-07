afa2bd2594f14b69008c0d110b60275bfdd043ba
gcalmettes <guillaume.calmettes@gmail.com>
2023-10-07 13:24:33 +0200

    feat: connect to stream

--- src/messaging/client.rs
+++ src/messaging/client.rs
@@ -1,13 +1,12 @@
 use reqwest::Client;
 
-// use futures::stream::{Stream, StreamExt};
-
+use futures::stream::StreamExt;
 use futures::stream::TryStreamExt; // for .map_err()
 use tokio::io::AsyncBufReadExt; // for .lines()
-use tokio_util::io::StreamReader;
+use tokio_util::io::StreamReader; // for .next()
 
 use crate::error::{convert_err, BotResult};
-use crate::messaging::models::Msg;
+use crate::messaging::models::{Command, Msg};
 
 pub struct MatterBridgeClient {
     http_client: Client,
@@ -23,75 +22,69 @@ impl MatterBridgeClient {
         }
     }
 
-    // async fn acquire_stream(
-    //     &self,
-    // ) -> BotResult<impl Stream<Item = Result<bytes::Bytes, reqwest::Error>>> {
-    //     let url = format!("{}{}", self.base_url, "/api/stream");
-
-    //     let stream = self.http_client.get(&url).send().await?.bytes_stream();
-
-    //     Ok(stream)
-    // }
-
-    // pub async fn read_stream(&self) -> BotResult<()> {
-    //     let mut stream = self.acquire_stream().await?;
-
-    //     while let Some(item) = stream.next().await {
-    //         // b"{\"text\":\"Hello xylophone\",\"channel\":\"api\",\"username\":\"gcalmettes\",\"userid\":\"\",\"avatar\":\"\",\"account\":\"api.myapi\",\"event\":\"\",\"protocol\":\"api\",\"gateway\":\"mygateway\",\"parent_id\":\"\",\"timestamp\":\"2023-09-16T13:45:55.303052409Z\",\"id\":\"\",\"Extra\":null}\n"
-
-    //         // let parsed_msg = serde_json::from_slice::<Msg>(&item.unwrap()).unwrap();
-    //         match item {
-    //             Ok(i) => {
-    //                 match serde_json::from_slice::<Msg>(&i) {
-    //                     Ok(msg) => {
-    //                         println!("MSG: {:?}", msg);
-    //                     }
-    //                     Err(e) => {
-    //                         println!("ERROR: {:?}", e);
-    //                     }
-    //                 };
-    //             }
-    //             Err(e) => {
-    //                 println!("ERROR: {:?}", e);
-    //             }
-    //         };
-    //     }
-
-    //     Ok(())
-    // }
-
-    // async fn acquire_stream(&self) -> BotResult<tokio::io::Lines<StreamReader<futures::stream::MapErr<impl Stream<Item = Result<bytes::Bytes, reqwest::Error>>, fn(reqwest::Error) -> std::io::Error {convert_error}>, bytes::Bytes>>> {
-    // async fn read_stream(
-    //     &self,
-    // ) -> BotResult<
-    //     tokio::io::Lines<
-    //         StreamReader<
-    //             futures::stream::MapErr<
-    //                 impl Stream<Item = Result<bytes::Bytes, reqwest::Error>>,
-    //                 std::io::Error,
-    //             >,
-    //             bytes::Bytes,
-    //         >,
-    //     >,
-    // > {
     pub async fn read_stream(&self) -> BotResult<()> {
         let url = format!("{}{}", self.base_url, "/api/stream");
 
-        let stream = self.http_client.get(&url).send().await?.bytes_stream();
+        let mut stream = self.http_client.get(&url).send().await?.bytes_stream();
 
-        let reader = StreamReader::new(stream.map_err(convert_err));
-        let mut lines = reader.lines();
-        while let Some(line) = lines.next_line().await? {
-            match serde_json::from_str::<Msg>(&line) {
-                Ok(msg) => {
-                    println!("MSG: {:?}", msg);
-                }
+        while let Some(item) = stream.next().await {
+            // b"{\"text\":\"Hello xylophone\",\"channel\":\"api\",\"username\":\"gcalmettes\",\"userid\":\"\",\"avatar\":\"\",\"account\":\"api.myapi\",\"event\":\"\",\"protocol\":\"api\",\"gateway\":\"mygateway\",\"parent_id\":\"\",\"timestamp\":\"2023-09-16T13:45:55.303052409Z\",\"id\":\"\",\"Extra\":null}\n"
+
+            // let parsed_msg = serde_json::from_slice::<Msg>(&item.unwrap()).unwrap();
+            match item {
+                Ok(msg) => match serde_json::from_slice::<Msg>(&msg) {
+                    Ok(msg) => match msg.as_command() {
+                        Ok(Command::Help) => {
+                            println!("Help command: {:?}", msg);
+                        }
+                        Ok(Command::Fast(_day)) => {
+                            println!("Fast command: {:?}", msg);
+                        }
+                        _ => {
+                            println!("NOT A COMMAND: {:?}", msg);
+                            ()
+                        }
+                    },
+                    Err(e) => {
+                        println!("ERROR: {:?}", e);
+                    }
+                },
                 Err(e) => {
                     println!("ERROR: {:?}", e);
                 }
-            }
+            };
         }
 
         Ok(())
     }
+
+    // pub async fn read_stream(&self) -> BotResult<()> {
+    //     let url = format!("{}{}", self.base_url, "/api/stream");
+
+    //     let stream = self.http_client.get(&url).send().await?.bytes_stream();
+
+    //     let reader = StreamReader::new(stream.map_err(convert_err));
+    //     let mut lines = reader.lines();
+    //     while let Some(line) = lines.next_line().await? {
+    //         match serde_json::from_str::<Msg>(&line) {
+    //             Ok(msg) => match msg.as_command() {
+    //                 Ok(Command::Help) => {
+    //                     println!("Help command: {:?}", msg);
+    //                 }
+    //                 Ok(Command::Fast(_day)) => {
+    //                     println!("Fast command: {:?}", msg);
+    //                 }
+    //                 _ => {
+    //                     println!("NOT A COMMAND: {:?}", msg);
+    //                     ()
+    //                 }
+    //             },
+    //             Err(e) => {
+    //                 println!("ERROR: {:?}", e);
+    //             }
+    //         }
+    //     }
+
+    //     Ok(())
+    // }
 }
--- src/messaging/models.rs
+++ src/messaging/models.rs
@@ -1,4 +1,60 @@
 use serde::Deserialize;
+use std::str::FromStr;
+
+#[derive(Debug)]
+pub enum Command {
+    Help,
+    Fast(Option<u8>),
+}
+
+impl FromStr for Command {
+    type Err = ();
+
+    fn from_str(s: &str) -> Result<Command, ()> {
+        if s.starts_with("!help") {
+            Ok(Command::Help)
+        } else if s.starts_with("!fast") {
+            match s.split_once(" ") {
+                Some((_, i)) => match i.parse::<u8>() {
+                    Ok(day) => Ok(Command::Fast(Some(day))),
+                    Err(e) => {
+                        println!("EEERRRR {e}");
+                        Err(())
+                    }
+                },
+                None => {
+                    println!("YYYYY");
+                    Ok(Command::Fast(None))
+                }
+            }
+        } else {
+            println!("NOPE {s}");
+            Err(())
+        }
+    }
+
+    // match s {
+    //     _ if s.starts_with("!help") => Ok(Command::Help),
+    //     _ if s.starts_with("!fast") => {
+    //         println!("GOOOGGG {}", s);
+    //         match s.split_once(" ") {
+    //             Some((_, i)) => match i.parse::<u8>() {
+    //                 Ok(day) => Ok(Command::Fast(Some(day))),
+    //                 Err(e) => {
+    //                     println!("EEERRRR {e}");
+    //                     Err(())
+    //                 }
+    //             },
+    //             None => {
+    //                 println!("YYYYY");
+    //                 Ok(Command::Fast(None))
+    //             }
+    //         }
+    //     }
+    //     _ => Err(()),
+    // }
+    // }
+}
 
 //  {
 //   "text": "world",
@@ -19,7 +75,7 @@ use serde::Deserialize;
 
 #[derive(Debug, Clone, Deserialize)]
 pub struct Msg {
-    text: String,
+    pub text: String,
     channel: String,
     username: String,
     userid: String,
@@ -32,4 +88,10 @@ pub struct Msg {
     timestamp: String,
     id: String,
     // extra: Option<HashMap<String, String>>,
+}
+
+impl Msg {
+    pub fn as_command(&self) -> Result<Command, ()> {
+        self.text.parse::<Command>()
+    }
 }
